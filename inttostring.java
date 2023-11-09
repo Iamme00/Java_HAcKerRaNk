ba5dcbf884ef92b4496cd2e184a9496e2a669d0a
Iamme00 <aavdarsha@gmail.com>
2023-11-09 22:28:05 +0530

    can use Integer.parseInt(var) to convert string to int and use Integer.toString(var) to convert int to string

---
+++ inttostring.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class inttostring {
+ public static void main(String[] args) {
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+    String s = Integer.toString(n);
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   }
+  } catch (Exception e) {
+   System.out.println("Please enter valid input");
+  }
+ }
+}
