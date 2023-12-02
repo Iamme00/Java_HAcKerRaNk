b8a0a1435512c39cd16d63b189e72d152eb73806
Iamme00 <aavdarsha@gmail.com>
2023-12-02 23:52:25 +0530

    array initialization

--- inttostring.java
+++ inttostring.java
@@ -2,12 +2,10 @@ import java.util.*;
 public class inttostring {
  public static void main(String[] args) {
   try {
-    
    Scanner in = new Scanner(System.in);
    int n = in .nextInt();
    in.close();
     String s = Integer.toString(n);
-   
    if (n == Integer.parseInt(s)) {
     System.out.println("Good job");
    }
